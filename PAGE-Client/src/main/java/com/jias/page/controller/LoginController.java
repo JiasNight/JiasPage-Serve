0d7bffd06a70d515bc0499a40b1e0a8af44a6398
JiasNight <Jias96@163.com>
2022-03-05 22:33:44 +0800

    xiugia

--- PAGE-Client/src/main/java/com/jias/page/controller/LoginController.java
+++ PAGE-Client/src/main/java/com/jias/page/controller/LoginController.java
@@ -6,22 +6,21 @@ import com.jias.page.utils.resultUtil.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin
 @RestController
 @Api(tags = "登陆相关")
-@RequestMapping("/login")
+@RequestMapping("/user")
 public class LoginController {
 
     @Autowired
     ILoginService loginService;
 
     @ApiOperation(value = "用户登陆", notes = "用户登陆")
-    @PostMapping("/into")
-    public Result singIn(@RequestParam("userName") String userName, @RequestParam("password") String password) {
+    @PostMapping("/login")
+    public Result singIn(@RequestParam("username") String userName, @RequestParam("password") String password) {
+        System.out.println("username="+userName+";password="+password);
         try {
             boolean isSigIn = loginService.userIsSigIn(userName, password);
             if (isSigIn) {
